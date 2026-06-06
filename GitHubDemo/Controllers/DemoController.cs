dbda3660273621cdb43ee1d22ab601bf50ee7349
Carolina Muñoz <cmunoz@baneco.com.bo>
2026-06-06 18:37:45 -0400

    primer commit

---
+++ GitHubDemo/Controllers/DemoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                mensaje = "Hola Mundo"
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("POST ejecutado");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok($"PUT ejecutado para {id}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"DELETE ejecutado para {id}");
+        }
+    }
+}
---
+++ GitHubDemoTest/DemoTestApi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace GitHubDemoTest
+{
+    public class SaludoTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public SaludoTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetSaludoDebeRetornar200()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/api/saludo");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
