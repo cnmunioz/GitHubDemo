3b607a7c6948bd39c1bfe468a7b90181e0e0d513
Carolina Muñoz <cmunoz@baneco.com.bo>
2026-06-07 18:54:16 -0400

    Adicionando pruebas

--- GitHubDemoTest/DemoTestApi.cs
+++ GitHubDemoTest/DemoTestApi.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Text.Json;
 
 namespace GitHubDemoTest
 {
@@ -26,5 +27,63 @@ namespace GitHubDemoTest
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
+
+        [Fact]
+        public async Task GetSaludoCorrecto()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/api/demo");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var contenido = await response.Content.ReadAsStringAsync();
+
+            using var doc = JsonDocument.Parse(contenido);
+
+            Assert.Equal(
+                "Hola Mundo",
+                doc.RootElement.GetProperty("mensaje").GetString()
+            );
+        }
+
+        [Fact]
+        public async Task Post_EjecucionCorrecta()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.PostAsync("/api/demo", null);
+
+            var contenido = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("POST ejecutado", contenido);
+        }
+
+        [Fact]
+        public async Task Put_Id()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.PutAsync("/api/demo/5", null);
+
+            var contenido = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("PUT ejecutado para 5", contenido);
+        }
+
+        [Fact]
+        public async Task Delete_Id()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.DeleteAsync("/api/demo/10");
+
+            var contenido = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("DELETE ejecutado para 10", contenido);
+        }
     }
+
+
+
 }
